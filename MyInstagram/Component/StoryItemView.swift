c261690a40c746270a0d78ed23b3df0e7329b423
anthony <aalbertini95@gmail.com>
2023-02-09 18:07:11 +0100

    Add StoryItemView

---
+++ MyInstagram/Component/StoryItemView.swift
@@ -0,0 +1,43 @@
+//
+//  StoryItemView.swift
+//  MyInstagram
+//
+//  Created by Anthony Albertini on 06/02/2023.
+//
+
+import SwiftUI
+
+struct StoryItemView: View {
+    @State var avatar: String
+    @State var nickname: String = ""
+
+    var body: some View {
+            VStack {
+                Button(action: {
+                    print("coucou")
+                }) {
+                    Image(avatar)
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .cornerRadius(60)
+                        .overlay(
+                            Circle()
+                                .stroke(LinearGradient(colors: [.purple, .red,.orange,.yellow], startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 2.5)
+                                .frame(width: 68, height: 68)
+                        )
+                            .padding(6)
+                }
+                
+                Text(nickname)
+                    .font(.caption)
+                    .foregroundColor(Settings.textColorScheme)
+            }
+        }
+}
+
+struct StoryItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryItemView(avatar: "image_1", nickname: "monkey_1")
+            .previewLayout(.sizeThatFits)
+    }
+}
