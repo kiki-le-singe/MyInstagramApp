2fd15a029144d9a5e4d97602c632cde6bbbe6b2f
anthony <aalbertini95@gmail.com>
2023-02-09 18:10:25 +0100

    Implement User Story

--- MyInstagram/View/HomeView.swift
+++ MyInstagram/View/HomeView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct HomeView: View {
-    var colors: [Color] = [.red, .blue, .green, .gray, .pink, .yellow, .orange, .purple]
+    let colors: [Color] = [.red, .blue, .green, .gray, .pink, .yellow, .orange, .purple]
+    let avatars: [User] = [
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_1", age: 2, gender: .male, avatar: "image_1"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_2", age: 4, gender: .female, avatar: "image_2"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_3", age: 2, gender: .male, avatar: "image_3"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_4", age: 5, gender: .male, avatar: "image_4"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_5", age: 1, gender: .female, avatar: "image_5"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_6", age: 3, gender: .female, avatar: "image_6"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_1", age: 2, gender: .male, avatar: "image_1"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_2", age: 4, gender: .female, avatar: "image_2"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_3", age: 2, gender: .male, avatar: "image_3"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_4", age: 5, gender: .male, avatar: "image_4"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_5", age: 1, gender: .female, avatar: "image_5"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_6", age: 3, gender: .female, avatar: "image_6"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_3", age: 2, gender: .male, avatar: "image_3"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_4", age: 5, gender: .male, avatar: "image_4"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_5", age: 1, gender: .female, avatar: "image_5"),
+        User(firstname: "Monkey 1", lastname: "Monkey", nickname: "monkey_6", age: 3, gender: .female, avatar: "image_6"),
+    ]
     
     var body: some View {
         NavigationStack {
             ScrollView {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    
+//                    LazyHStack(spacing: 12) {
+                    HStack(spacing: 12) {
+                        ForEach(avatars, id: \.self) { user in
+                            StoryItemView(avatar: user.avatar, nickname: user.nickname ?? "")
+                        }
+                    }
+                }
+                .padding(8)
+                
                 VStack(spacing: 20, content: {
                     ForEach(colors, id: \.self) { color in
                         color.frame(height: 200).cornerRadius(20)
